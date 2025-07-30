bbffd2836b390a9aa62cf013173c13884b4d7067
AdrisJ6 <adris@elefant.gg>
2025-07-30 15:07:10 -0400

    Make it so comments are auto read and stored, great! We have the original functionality but it works on build and can be read. Nice!

--- dev_scenes/simple_chat/ai_simple_tool_receiver.gd
+++ dev_scenes/simple_chat/ai_simple_tool_receiver.gd
@@ -1,20 +1,21 @@
 extends Node
-
 @export var billboard : Label
 @export var blink_background : ColorRect
 
-## Blinks the screen
+## Make the background of the user's input field quickly flash
+## black and then go back to normal.
 func blink() -> void:
 	print("blinked!")
 	var c = blink_background.color
 	blink_background.color = Color.BLACK
 	blink_background.create_tween().tween_property(blink_background, "color", c, 0.5).set_trans(Tween.TRANS_SINE)
 
-## Gets the time in the format YYYY-MM-DDTHH:MM:SS
+## Gets the time in full string format (YYYY-MM-DDTHH:MM:SS).
 func get_time() -> String:
 	return Time.get_datetime_string_from_system()
 
-## Announces a message by placing it on a billboard
+## Set the text of a big label at the center of the screen.
+## Label keeps this value until announce is called again.
 func announce(announcement : String) -> void:
 	print("announcing: " + announcement)
 	billboard.text = announcement
