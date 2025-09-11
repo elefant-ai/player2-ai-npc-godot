4bb1576ff9d3bcf1f8d4b027f6dade15ac3201fb
AdrisJ6 <adris@elefant.gg>
2025-09-11 17:09:38 -0400

    blink, add back in

--- templates/chat/tool_calling.gd
+++ templates/chat/tool_calling.gd
@@ -10,9 +10,18 @@ extends Node
 func console_log_hello_world():
 	print("Hello World!")
 
-## Logs a custom message to the console
-func console_log(message : String):
-	print("CONSOLE LOGGING:", message)
+## Make the background of the user's input field quickly flash
+## black and then go back to normal. Notifies caller when one blink is successful.
+func blink() -> String:
+	print("blinked!")
+	var blink_background = $"../Simple Interface"
+	var c = blink_background.color
+	blink_background.color = Color.BLACK
+	blink_background.create_tween().tween_property(blink_background, "color", c, 0.5).set_trans(Tween.TRANS_SINE)
+	await get_tree().create_timer(1).timeout
+	print("done.")
+	return "Finished blinking."
+
 
 ## Gets the time in full string format (YYYY-MM-DDTHH:MM:SS).
 func get_current_time():
