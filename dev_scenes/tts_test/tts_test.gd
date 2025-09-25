8db406a90b472c88f49e187012caa65d15006fd1
AdrisJ6 <adris@elefant.gg>
2025-09-25 18:42:57 -0400

    (beef up TTS test scene)

--- dev_scenes/tts_test/tts_test.gd
+++ dev_scenes/tts_test/tts_test.gd
@@ -1,6 +1,22 @@
 extends Node
 
 func _ready() -> void:
-	$Button.pressed.connect(func():
-		$Player2TTS.speak($TextEdit.text)
+	var p2tts : Player2TTS = $Player2TTS
+	var text : TextEdit = $VBoxContainer/TextEdit
+	var speak_button : Button = $VBoxContainer/Button
+	var voice_text : TextEdit = $"VBoxContainer/HBoxContainer/Voice ID"
+	var stream_toggle : CheckButton = $VBoxContainer/HBoxContainer/CheckButton
+
+	voice_text.text = p2tts.config.voice_id
+	voice_text.text_changed.connect(func():
+		p2tts.config.voice_id = voice_text.text
+	)
+	stream_toggle.button_pressed = p2tts.config.stream
+	stream_toggle.pressed.connect(func():
+		print("STREAM SET: ", stream_toggle.button_pressed)
+		p2tts.config.stream = stream_toggle.button_pressed
 		)
+
+	speak_button.pressed.connect(func():
+		p2tts.speak(text.text)
+	)
