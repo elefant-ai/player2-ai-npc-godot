34a261aafad98607c7264905ad00138fa5455e83
Chris Green <chris@elefant.gg>
2026-03-02 13:59:43 -0500

    feat: add _req_with_game_id method for data endpoints

--- addons/player2/helpers/api.gd
+++ addons/player2/helpers/api.gd
@@ -291,7 +291,73 @@ func _req_stream(path_property : String, method: HTTPClient.Method = HTTPClient.
 		)
 
 	# Run the auth
-	_prereq_auth(on_auth_ready, on_fail)	
+	_prereq_auth(on_auth_ready, on_fail)
+
+## Like _req but replaces {game_id} in the path with the client_id
+func _req_with_game_id(path_property: String, method: HTTPClient.Method = HTTPClient.Method.METHOD_GET, body: Variant = "", on_completed: Callable = Callable(), on_fail: Callable = Callable()):
+	var game_id = ProjectSettings.get_setting("player2/client_id", "")
+	if game_id.is_empty():
+		var msg = "No client id defined. Cannot make game data request."
+		Player2ErrorHelper.send_error(msg)
+		if on_fail.is_valid():
+			on_fail.call(msg, -2)
+		return
+
+	var api := Player2APIConfig.grab()
+	var use_web = using_web()
+	var endpoint = api.endpoint_web if use_web else api.endpoint_local
+
+	# Get the raw path and replace game_id
+	var path: String = endpoint.get(path_property)
+	if path:
+		path = path.replace("{root}", endpoint.get("root"))
+		path = path.replace("{game_id}", game_id)
+
+	print("hitting path (with game_id) ", path)
+
+	var on_auth_ready = func(run_again):
+		Player2WebHelper.request(
+			path,
+			method,
+			body,
+			_get_headers(use_web),
+			func(response_body, code, headers):
+				if !_code_success(code):
+					if use_web and code == 401:
+						if _internal_site:
+							Player2ErrorHelper.send_error("Got Unauthorized response. Try refreshing the page!")
+							return
+						print("Unauthorized response. Resetting key and trying to re-auth.")
+						Player2ErrorHelper.send_error("Got Unauthorized while doing web requests, redoing auth.")
+						_web_p2_key = ""
+						run_again.call()
+						return
+					_alert_error_fail(code, false, response_body)
+					if on_fail.is_valid():
+						on_fail.call(response_body, code)
+					return
+				if on_completed.is_valid():
+					var result = JSON.parse_string(response_body)
+					on_completed.call(result if result else response_body)
+				request_success.emit(path_property),
+			func(response_body, code):
+				if code != HTTPRequest.RESULT_SUCCESS:
+					if use_web:
+						_last_web_present = false
+					else:
+						_last_local_present = false
+					if !_last_local_present and !_last_web_present:
+						_source_tested = false
+						print("Source got unset. Trying to find again...")
+						Player2AsyncHelper.call_timeout(run_again, 3)
+				else:
+					_last_web_present = true
+				_alert_error_fail(code, true)
+				if on_fail.is_valid():
+					on_fail.call("", code)
+		)
+
+	_prereq_auth(on_auth_ready, on_fail)
 
 func _prereq_auth(on_auth_ready : Callable, on_fail : Callable = Callable()):
 	var api := Player2APIConfig.grab()
